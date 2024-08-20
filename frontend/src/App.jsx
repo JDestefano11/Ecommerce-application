61ab7f4fd2794f64a23e252a141f5eabd607d562
DesJCodes <141986568+DesJCodes@users.noreply.github.com>
2024-08-20 15:49:34 -0400

    "Updated App.jsx to include routing, added imports and routes for various pages, removed Card.jsx, and updated main.jsx to use BrowserRouter"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Collections from "./pages/Collections";
+import Cart from "./pages/Cart";
+import PlaceOrder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
+import Orders from "./pages/Orders";
+import Login from "./pages/Login";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg-px[9vw]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collections" element={<Collections />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" elemnt={<Login />} />
+        <Route path="/placeorder" element={<PlaceOrder />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
